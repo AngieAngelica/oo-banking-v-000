f65751e8b8841e1672a695efc72d59dac398013f
Angie Kassab <anasab12@gmail.com>
2018-08-19 01:21:58 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,15 +14,15 @@ class Transfer
   end
   
   def execute_transaction
-    unless status == "complete" # to make sure the transaction executes only one time
-    sender.balance -= amount
-    receiver.balance += amount
-    self.status = "complete"
+    unless status == "complete" # to make sure the transaction    executes only one time
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
     
-    if sender.valid? == false # or !sender.valid?
-      self.status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end
+      if sender.valid? == false # or !sender.valid?
+        self.status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      end
     end
    end
    
