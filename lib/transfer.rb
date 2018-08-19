ca489abdebedaa0748b4c88b9f72dcf55129826c
Angie Kassab <anasab12@gmail.com>
2018-08-19 00:10:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,11 @@
 class Transfer
   
-  attr_accessor :account1, :account2, :amount
+  attr_accessor :account1, :account2, :amount :sender
   
   def initialize( account1, account2, amount)
     @account1 = account1
     @account2 = account2
     @amount = amount
+    @sender = sender
   end
 end
